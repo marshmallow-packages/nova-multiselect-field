b2e94d71d5b8259ed9a7b9c87eed245447822a73
LTKort <lars.kort94@gmail.com>
2026-09-07 17:46:28 +0200

    chore: stop tracking committed vendor directory

--- vendor/laravel/nova/routes/asset.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use Laravel\Nova\Http\Controllers\ScriptController;
-use Laravel\Nova\Http\Controllers\StyleController;
-
-// Scripts & Styles...
-Route::get('/scripts/{script}', ScriptController::class);
-Route::get('/styles/{style}', StyleController::class);
--- vendor/laravel/nova/src/Query/Search/SearchType.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Laravel\Nova\Query\Search;
-
-enum SearchType
-{
-    case EXACT;
-    case LIKE;
-}
